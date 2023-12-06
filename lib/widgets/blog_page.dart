415d34ee3082ecdfdfbfcf35a55f545159aed464
Ekbel <zrellliekbel@gmail.com$>
2023-12-06 02:44:39 +0100

    commentaire

--- lib/widgets/blog_page.dart
+++ lib/widgets/blog_page.dart
@@ -6,6 +6,30 @@ void main() {
   ));
 }
 
+class Blog {
+  String title;
+  String type;
+  String creationDate;
+  String image;
+  String dateDePrevention;
+  String region;
+  String pays;
+  String statement;
+  String description;
+
+  Blog({
+    required this.title,
+    required this.type,
+    required this.creationDate,
+    required this.image,
+    required this.dateDePrevention,
+    required this.region,
+    required this.pays,
+    required this.statement,
+    required this.description,
+  });
+}
+
 class BlogPage extends StatelessWidget {
   const BlogPage({Key? key}) : super(key: key);
 
@@ -24,6 +48,9 @@ class BlogPage extends StatelessWidget {
             width: double.infinity,
             height: double.infinity,
           ),
+          Container(
+            color: Colors.lightBlue.withOpacity(0.5), // Background color with transparency
+          ),
           // Search Bar
           Padding(
             padding: const EdgeInsets.all(16.0),
@@ -41,7 +68,7 @@ class BlogPage extends StatelessWidget {
           // Cadre Blanc Non Transparent
           Container(
             decoration: BoxDecoration(
-              color: Colors.white,
+              color: Colors.white.withOpacity(0.8), // Adjust transparency as needed
               borderRadius: BorderRadius.circular(12.0),
             ),
             margin: const EdgeInsets.fromLTRB(16, 80, 16, 16),
@@ -61,20 +88,34 @@ class BlogPage extends StatelessWidget {
                 ),
                 const SizedBox(height: 8),
                 // Table Rows (Example)
-                _buildTableRow('Blog Title 1', 'Natural Disaster', '2023-12-05', context,
+                _buildTableRow(
+                  Blog(
+                    title: 'Blog Title 1',
+                    type: 'Natural Disaster',
+                    creationDate: '2023-12-05',
                     image: 'Image URL',
                     dateDePrevention: 'Prevention Date',
                     region: 'Region',
                     pays: 'Country',
                     statement: 'Statement',
-                    description: 'Description'),
-                _buildTableRow('Blog Title 2', 'Man-Made Disaster', '2023-12-06', context,
+                    description: 'Description',
+                  ),
+                  context,
+                ),
+                _buildTableRow(
+                  Blog(
+                    title: 'Blog Title 2',
+                    type: 'Man-Made Disaster',
+                    creationDate: '2023-12-06',
                     image: 'Image URL',
                     dateDePrevention: 'Prevention Date',
                     region: 'Region',
                     pays: 'Country',
                     statement: 'Statement',
-                    description: 'Description'),
+                    description: 'Description',
+                  ),
+                  context,
+                ),
                 // Ajoutez plus de lignes au besoin
                 const SizedBox(height: 16),
               ],
@@ -85,26 +126,21 @@ class BlogPage extends StatelessWidget {
     );
   }
 
-  Widget _buildTableRow(String title, String type, String creationDate, BuildContext context,
-      {required String image,
-      required String dateDePrevention,
-      required String region,
-      required String pays,
-      required String statement,
-      required String description}) {
+  Widget _buildTableRow(Blog blog, BuildContext context) {
     return Column(
       children: [
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text(title, style: TextStyle(color: Colors.black)),
-            Text(type, style: TextStyle(color: Colors.black)),
-            Text(creationDate, style: TextStyle(color: Colors.black)),
+            Text(blog.title, style: TextStyle(color: Colors.black)),
+            Text(blog.type, style: TextStyle(color: Colors.black)),
+            Text(blog.creationDate, style: TextStyle(color: Colors.black)),
             Row(
               children: [
                 IconButton(
                   onPressed: () {
                     // Gérer le bouton Éditer
+                    _showEditForm(context, blog);
                   },
                   icon: Icon(Icons.edit, color: Colors.green),
                 ),
@@ -117,20 +153,14 @@ class BlogPage extends StatelessWidget {
                 IconButton(
                   onPressed: () {
                     // Gérer le bouton Détails
-                    _showDetailsDialog(context, title, type, creationDate,
-                        image: image,
-                        dateDePrevention: dateDePrevention,
-                        region: region,
-                        pays: pays,
-                        statement: statement,
-                        description: description);
+                    _showDetailsDialog(context, blog);
                   },
                   icon: Icon(Icons.details, color: Colors.blue),
                 ),
                 IconButton(
                   onPressed: () {
                     // Gérer le bouton Commentaire
-                    _showCommentsDialog(context, title);
+                    _showCommentsDialog(context, blog.title);
                   },
                   icon: Icon(Icons.comment, color: Colors.red),
                 ),
@@ -143,13 +173,45 @@ class BlogPage extends StatelessWidget {
     );
   }
 
-  void _showDetailsDialog(BuildContext context, String title, String type, String creationDate,
-      {required String image,
-      required String dateDePrevention,
-      required String region,
-      required String pays,
-      required String statement,
-      required String description}) {
+  void _showEditForm(BuildContext context, Blog blog) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Edit Blog'),
+          content: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              // Include form fields for editing
+              // You can use TextFormField, etc.
+              Text('Title: ${blog.title}'),
+              Text('Type of Catastrophe: ${blog.type}'),
+              // Add more form fields as needed
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                // Handle save button press
+                Navigator.of(context).pop();
+              },
+              child: Text('Save'),
+            ),
+            TextButton(
+              onPressed: () {
+                // Handle cancel button press
+                Navigator.of(context).pop();
+              },
+              child: Text('Cancel'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showDetailsDialog(BuildContext context, Blog blog) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
@@ -159,14 +221,14 @@ class BlogPage extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisSize: MainAxisSize.min,
             children: [
-              Text('Titre : $title'),
-              Text('Image: $image'),
-              Text('Type de Catastrophe : $type'),
-              Text('Date de Prevention: $dateDePrevention'),
-              Text('region: $region'),
-              Text('pays: $pays'),
-              Text('Statement: $statement'),
-              Text('Description: $description'),
+              Text('Titre : ${blog.title}'),
+              Text('Image : ${blog.image}'),
+              Text('Type de Catastrophe : ${blog.type}'),
+              Text('Date de Prevention: ${blog.dateDePrevention}'),
+              Text('Region: ${blog.region}'),
+              Text('Pays: ${blog.pays}'),
+              Text('Statement: ${blog.statement}'),
+              Text('Description: ${blog.description}'),
               // Ajoutez plus de détails au besoin
             ],
           ),
@@ -184,35 +246,82 @@ class BlogPage extends StatelessWidget {
   }
 
   void _showCommentsDialog(BuildContext context, String title) {
-    // Exemple de liste de commentaires
-    List<String> comments = [
-      'Commentaire 1',
-      'Commentaire 2',
-      'Commentaire 3',
-    ];
+  // Exemple de liste de commentaires avec identifiants uniques
+  List<Map<String, String>> comments = [
+    {'comment': 'Excellent article!', 'id': '1'},
+    {'comment': 'Thanks for sharing!', 'id': '2'},
+    {'comment': 'Interesting insights.', 'id': '3'},
+  ];
 
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Commentaires pour $title'),
-          content: Column(
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return Dialog(
+        backgroundColor: Colors.transparent,
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(12.0),
+          ),
+          padding: const EdgeInsets.all(16),
+          child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisSize: MainAxisSize.min,
             children: [
-              for (String comment in comments) Text(comment),
+              Text(
+                'Commentaires pour $title',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 8),
+              Divider(
+                color: Colors.black,
+                height: 1,
+              ),
+              const SizedBox(height: 8),
+              for (Map<String, String> comment in comments)
+                _buildCommentRow(comment),
+              const SizedBox(height: 8),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text(
+                      'Fermer',
+                      style: TextStyle(color: Colors.blue),
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('Fermer'),
-            ),
-          ],
-        );
-      },
-    );
-  }
+        ),
+      );
+    },
+  );
+}
+
+Widget _buildCommentRow(Map<String, String> comment) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+      Row(
+        children: [
+          Icon(Icons.account_circle, size: 40, color: Colors.blue),
+          const SizedBox(width: 8),
+          Text(comment['comment'] ?? ''),
+        ],
+      ),
+      IconButton(
+        onPressed: () {
+          // Ajoutez ici la logique pour supprimer le commentaire avec l'ID associé
+          print('Supprimer le commentaire avec ID ${comment['id']}');
+        },
+        icon: Icon(Icons.delete, color: Colors.red),
+      ),
+    ],
+  );
+}
 }
