0335e42ce3c54da0f0d758a55305c4273d74e4fd
dhiasaibi <dhiasaibi@yahoo.com>
2023-12-06 08:29:34 +0100

    linechart done

--- lib/widgets/recent_files.dart
+++ lib/widgets/recent_files.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:garduino_dashboard/model/RecentFile.dart';
+import 'package:garduino_dashboard/pages/home/widgets/line_chart_card.dart';
 
 class RecentFiles extends StatelessWidget {
   const RecentFiles({
@@ -9,48 +10,51 @@ class RecentFiles extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        color: Color(0xFF2A2D3E),
-        borderRadius: const BorderRadius.all(Radius.circular(10)),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            "Liste des catastrophes",
-            style: Theme.of(context).textTheme.titleMedium,
-          ),
-          SizedBox(
-            width: double.infinity,
-            child: DataTable(
-              columnSpacing: 16,
-              // minWidth: 600,
-              columns: [
-                DataColumn(
-                  label: Text("Description"),
+    return SingleChildScrollView(
+      child: Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: Color(0xFF2A2D3E),
+          borderRadius: const BorderRadius.all(Radius.circular(10)),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Liste des catastrophes",
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: DataTable(
+                columnSpacing: 16,
+                // minWidth: 600,
+                columns: [
+                  DataColumn(
+                    label: Text("Description"),
+                  ),
+                  DataColumn(
+                    label: Text("Date"),
+                  ),
+                  DataColumn(
+                    label: Text("Magnitude"),
+                  ),
+                  DataColumn(
+                    label: Text("Supprimer"),
+                  ),
+                  DataColumn(
+                    label: Text("Modifier"),
+                  ),
+                ],
+                rows: List.generate(
+                  demoRecentFiles.length,
+                  (index) => recentFileDataRow(demoRecentFiles[index]),
                 ),
-                DataColumn(
-                  label: Text("Date"),
-                ),
-                DataColumn(
-                  label: Text("Magnitude"),
-                ),
-                DataColumn(
-                  label: Text("Supprimer"),
-                ),
-                DataColumn(
-                  label: Text("Modifier"),
-                ),
-              ],
-              rows: List.generate(
-                demoRecentFiles.length,
-                (index) => recentFileDataRow(demoRecentFiles[index]),
               ),
             ),
-          ),
-        ],
+            LineChartCard()
+          ],
+        ),
       ),
     );
   }
